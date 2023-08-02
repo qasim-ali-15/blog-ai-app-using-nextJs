6615d957f8d72b362d328c604cbb7c700c31f787
Qasim ALi <119096357+qasim-ali-15@users.noreply.github.com>
2023-08-02 16:15:23 +0500

    tech-travel-other-sidebar-prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,34 @@
+import Tech from './(home)/Tech';
 import Trending from './(home)/Trending'
+import Travel from './(home)/Travel'
+import Other from './(shared)/Other';
+import Subscribe from './(shared)/Subscribe';
+import Sidebar from './(shared)/Sidebar';
+import { BrowserRouter } from 'react-router-dom'
 
 export default function Home() {
-  return <main>
+  return (
+  
+  <main className='mx-5'>
     <Trending />
       <div className="md:flex gap-10 mb-5">
         <div className="basis-3/4">
+          <Tech />
+          <Travel />
+          <Other />
           {/* <Tech techPosts={techPosts} /> */}
           {/* <Travel travelPosts={travelPosts} />
           <Other otherPosts={otherPosts} /> */}
           <div className="hidden md:block">
+            <Subscribe/>
             {/* <Subscribe /> */}
           </div>
         </div>
         <div className="basis-1/4">
+          <Sidebar/>
           {/* <Sidebar /> */}
         </div>
       </div>
-  </main>;
+  </main>
+  );
 }
