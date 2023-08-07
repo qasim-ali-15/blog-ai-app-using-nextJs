09083158112763fa50da4b45e9c9859fcc273a2a
Qasim ALi <119096357+qasim-ali-15@users.noreply.github.com>
2023-08-07 10:35:52 +0500

    heroicons-svg

--- app/post/[id]/CategoryAndEdit.tsx
+++ app/post/[id]/CategoryAndEdit.tsx
@@ -1,7 +1,7 @@
 import { FormattedPost } from "@/app/types";
 import { Editor } from "@tiptap/react";
 import React from "react";
-import { XMarkIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
+// import { XMarkIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
 
 type Props = {
   isEditable: boolean;
@@ -49,12 +49,35 @@ const CategoryAndEdit = ({
         {isEditable ? (
           <div className="flex justify-between gap-3">
             <button onClick={handleCancelEdit}>
-              <XMarkIcon className="h-6 w-6 text-accent-red" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6 text-accent-red"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+
+              {/* <XMarkIcon className="h-6 w-6 text-accent-red" /> */}
             </button>
           </div>
         ) : (
           <button onClick={handleEnableEdit}>
-            <PencilSquareIcon className="h-6 w-6 text-accent-red" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 text-accent-red"
+            >
+              <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+              <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+            </svg>
+
+            {/* <PencilSquareIcon className="h-6 w-6 text-accent-red" /> */}
           </button>
         )}
       </div>
