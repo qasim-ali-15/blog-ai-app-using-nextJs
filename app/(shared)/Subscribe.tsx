2cc0d29d0eb691d082bf0754a1893e97759340d9
Qasim ALi <119096357+qasim-ali-15@users.noreply.github.com>
2023-08-08 18:08:42 +0500

    formatting

--- app/(shared)/Subscribe.tsx
+++ app/(shared)/Subscribe.tsx
@@ -8,7 +8,7 @@ const Subscribe = () => {
         Enter email address to get top news and great deals
       </div>
       <input
-        className="text-center w-5/6 min-w-[100px] px-5 py-2 border-2"
+        className="text-center w-5/6 min-w-[100px] px-5 py-2 border-2 focus:outline-none focus:border focus:border-accent-orange"
         placeholder="Enter Email Address"
       />
       <button className="bg-accent-orange opacity-100 hover:opacity-90 hover:font-bold text-white font-semibold w-5/6 min-w-[100px] py-2 px-5 mt-3">
