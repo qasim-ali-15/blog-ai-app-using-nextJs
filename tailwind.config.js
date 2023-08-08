751fd686c9052362d4a52313b3e3087533dacf9e
Qasim ALi <119096357+qasim-ali-15@users.noreply.github.com>
2023-08-08 15:10:42 +0500

    formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         "wh-500": "#595959",
         "wh-900": "#0F0F0F",
         "accent-red": "#EA9648",
-        "accent-orange": "#F6CF68",
+        "accent-orange": "#F5C23D",
         "accent-green": "#C2E9B4",
       },
       backgroundImage: {
